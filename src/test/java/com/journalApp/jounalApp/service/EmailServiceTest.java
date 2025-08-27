20e0a7bd387b7cef1987af168fd009bff65a22a9
vivek <vivan100123@gmail.com>
2025-08-28 01:17:58 +0530

    Email service tested successfully

---
+++ src/test/java/com/journalApp/jounalApp/service/EmailServiceTest.java
@@ -0,0 +1,17 @@
+package com.journalApp.jounalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail(){
+        emailService.sendEmail("Enter-user's mail here.","Java send email testing.","Hello, how are you?");
+    }
+}
